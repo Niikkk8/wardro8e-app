094de192d58a47b9c3ccb54f2433a923ef47251c
Niket <shahniket643@gmail.com>
2026-03-14 06:05:43 +0530

    because it never works on the second try either

--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   dependencies: {
-    // These packages are not used in this project and have caused
-    // Gradle configuration-phase failures that break ALL native module builds.
-    // Exclude them from autolinking even if installed as transitive deps.
-    'react-native-worklets': {
-      platforms: { android: null, ios: null },
-    },
+    // react-native-worklets-core is a transitive dep of react-native-reanimated@3.x
+    // but is NOT needed for reanimated@4.x which uses react-native-worklets instead.
+    // Excluding it prevents autolinking conflicts.
     'react-native-worklets-core': {
       platforms: { android: null, ios: null },
     },
